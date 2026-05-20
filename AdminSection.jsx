30f8e6df4cb0b53f1d7529e15cb1be4e22fd463d
Fraer <1stbogatyr@gmail.com>
2026-05-20 15:17:16 +0300

    Delete AdminSection.jsx

--- AdminSection.jsx
+++
@@ -1,9 +0,0 @@
-function AdminEmptyState({ text }) {
-  return (
-    <div className="rounded-lg border border-dashed border-slate-300 bg-white p-6 text-center text-sm text-slate-500">
-      {text}
-    </div>
-  )
-}
-
-export default AdminEmptyState
